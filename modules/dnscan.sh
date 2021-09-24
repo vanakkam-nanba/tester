5fa09fdd16909e6dafddd18235858a6f46f8216c
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-09-24 20:28:03 +0530

    Update dnscan.sh

--- modules/dnscan.sh
+++ modules/dnscan.sh
@@ -2,21 +2,21 @@
 
 url=$1
 
-echo "1) DNSCAN :" | tee -a ../results/$url/output.txt
-printf "\n\n" >> ../results/$url/output.txt
+echo "1) DNSCAN :" | tee -a /app/results/$url/output.txt
+printf "\n\n" >> /app/results/$url/output.txt
 
-host -t ns $url | tee -a ../results/$url/output.txt
-printf "\n" >> ../results/$url/output.txt
-host -t mx $url | tee -a ../results/$url/output.txt
-printf "\n" >> ../results/$url/output.txt
-host -t soa $url | tee -a ../results/$url/output.txt
-printf "\n" >> ../results/$url/output.txt
-host -t dnskey $url | tee -a ../results/$url/output.txt
-printf "\n" >> ../results/$url/output.txt
-host -t aaaa $url | tee -a ../results/$url/output.txt
-printf "\n" >> ../results/$url/output.txt
-host -t cname $url | tee -a ../results/$url/output.txt
+host -t ns $url | tee -a /app/results/$url/output.txt
+printf "\n" >> /app/results/$url/output.txt
+host -t mx $url | tee -a /app/results/$url/output.txt
+printf "\n" >> /app/results/$url/output.txt
+host -t soa $url | tee -a /app/results/$url/output.txt
+printf "\n" >> /app/results/$url/output.txt
+host -t dnskey $url | tee -a /app/results/$url/output.txt
+printf "\n" >> /app/results/$url/output.txt
+host -t aaaa $url | tee -a /app/results/$url/output.txt
+printf "\n" >> /app/results/$url/output.txt
+host -t cname $url | tee -a /app/results/$url/output.txt
 
-printf "\n\n" >> ../results/$url/output.txt
-printf "############################################################" >> ../results/$url/output.txt
-printf "\n\n" >> ../results/$url/output.txt
+printf "\n\n" >> /app/results/$url/output.txt
+printf "############################################################" >> /app/results/$url/output.txt
+printf "\n\n" >> /app/results/$url/output.txt
