8cae95f4e72878d60800125852acd8d69cf63c82
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-09-24 20:24:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,18 +9,18 @@ bot = telebot.TeleBot(os.environ['API_KEY'])
 chat_id = os.environ['CHAT_ID']
 
 #creating a directory in the name of target to save all results
-os.mkdir("results/{}".format(url))
+os.mkdir("/app/results/{}".format(url))
 
 #dnscan
-os.system('bash modules/dnscan.sh')
+os.system('bash /app/modules/dnscan.sh')
 bot.send_message(chat_id, f"dnscan for {url} completed !")
 
 #subdomain_enumeration
-os.system('bash modules/subdomains.sh')
+os.system('bash /app/modules/subdomains.sh')
 bot.send_message(chat_id, f"subdomain enumeration for {url} completed !")
 
 #urls_gather
-os.system('bash modules/gather_urls.sh')
+os.system('bash /app/modules/gather_urls.sh')
 bot.send_message(chat_id, f"gathered all urls for {url} !")
 
 
