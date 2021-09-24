77c70768af77ba4f6798493c653fa78907593750
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-09-25 00:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,26 @@ def adder():
 def results():
    return "Results Here!"
 
+@app.route('/test', defaults={'req_path': ''})
+@app.route('/test/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = '/Users/vivek/Desktop'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+
 @app.route("/initialise")
 def initialiser():
    os.system("python /app/initialise.py")
