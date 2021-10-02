7ca2bdc7d82ef590c3adfcecf929aa84b9069df3
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-02 11:26:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_file, request
 from bp import auto_bp
 import os
+import psycopg2
 
 app = Flask(__name__)
 app.register_blueprint(auto_bp, url_prefix='/results')
@@ -14,41 +15,39 @@ def home():
 def adder():
  if request.method == "POST":
   url = request.form.get('url')
-  f = open('/app/domains.txt','a')
-  f.write(url)
-  f.write("\n")
-  f.close()
+  DATABASE_URL = os.environ['DATABASE_URL']
+
+  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+  cur = conn.cursor()
+
+  cur.execute(f"insert into queue ('target',) values ('{url}')")
+  conn.commit()
+
+  cur.close()
+  conn.close()
+
   return render_template("index.html", data="Added to queue !")
+
+@app.route("/queue")
+def check_queue():
+  str = '<h1>Targets in queue are</h1><br><br><ul>'
+  DATABASE_URL = os.environ['DATABASE_URL']
+  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+  cur = conn.cursor()
+  cur.execute("select * from queue")
+  t = cur.fetchall()
+  for i in t:
+    str = str + '<li> {}'.format(i[1])
+  str = str + "</ul>"
+  conn.commit()
+  cur.close()
+  conn.close()
+
+  return str
 
 @app.route("/results")
 def results():
    return "Results Here!"
 
-@app.route('/test', defaults={'req_path': ''})
-@app.route('/test/<path:req_path>')
-def dir_listing(req_path):
-    BASE_DIR = '/app/results'
-
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        return abort(404)
-
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
-
-    # Show directory contents
-    files = os.listdir(abs_path)
-    return render_template('files.html', files=files)
-
-@app.route("/initialise")
-def initialiser():
-   os.system("python /app/initialise.py")
-   return "sucess !!"
-
-
 if __name__ == "__main__":
-  app.run(port=8000, debug=True)
+  app.run(port=8000)
