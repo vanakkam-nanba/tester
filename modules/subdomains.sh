c6ecb53feb8e2ae32b26c215b0333df5ff0c335e
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-02 20:59:05 +0530

    Update subdomains.sh

--- modules/subdomains.sh
+++ modules/subdomains.sh
@@ -4,19 +4,15 @@
 url=$1
 
 /app/modules/binaries/subfinder -silent -d $url > /app/sub1
-
-#curl -s "https://crt.sh/?q=$url" | grep "<TD>" | grep $url | cut -d ">" -f2 | cut -d "<" -f1 | sort -u | sed '/^*/d' > sub3
-#curl -s "https://rapiddns.io/subdomain/$url#result" | grep "<td><a" | cut -d '"' -f 2 | grep http | cut -d '/' -f3 | sort -u > sub4
-#curl -s "https://dns.bufferover.run/dns?q=.$url" | jq -r .FDNS_A[] | cut -d '\' -f2 | cut -d "," -f2 |  sort -u > sub5
-#curl -s "https://riddler.io/search/exportcsv?q=pld:$url" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u > sub6
-#curl -s "https://jldc.me/anubis/subdomains/$url" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | cut -d "/" -f3 > sub7
-#curl -s "https://sonar.omnisint.io/subdomains/$url" | cut -d "[" -f1 | cut -d "]" -f1 | cut -d "\"" -f 2 > sub8
+curl -s "https://crt.sh/?q=$url" | grep "<TD>" | grep $url | cut -d ">" -f2 | cut -d "<" -f1 | sort -u | sed '/^*/d' > /app/sub2
+curl -s "https://riddler.io/search/exportcsv?q=pld:$url" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u > /app/sub3
+curl -s "https://jldc.me/anubis/subdomains/$url" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | cut -d "/" -f3 > /app/sub4
 
 
 echo "2) SUBDOMAINS :" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-sort /app/sub1 | uniq | tee -a /app/results/$url-output.txt
+sort /app/sub1 /app/sub2 /app/sub3 /app/sub4 | uniq | tee -a /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "######################################################################" >> /app/results/$url-output.txt
