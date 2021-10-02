6847e7273523c9dfcb11c2b0265b79882f01d389
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-02 20:34:56 +0530

    Update dnscan.sh

--- modules/dnscan.sh
+++ modules/dnscan.sh
@@ -17,6 +17,7 @@ host -t aaaa $url | tee -a /app/results/$url-output.txt
 printf "\n" >> /app/results/$url-output.txt
 host -t cname $url | tee -a /app/results/$url-output.txt
 
-printf "\n\n" >> /app/results/$url-output.txt
-printf "############################################################" >> /app/results/$url-output.txt
-printf "\n\n" >> /app/results/$url-output.txt
+printf "\n\n\n" >> /app/results/$url-output.txt
+printf "######################################################################" >> /app/results/$url-output.txt
+printf "######################################################################" >> /app/results/$url-output.txt
+printf "\n\n\n" >> /app/results/$url-output.txt
