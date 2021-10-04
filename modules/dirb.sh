6d4bb0d46f8fd442a9312f263adf031c3a6f55ce
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-04 12:06:41 +0530

    Update dirb.sh

--- modules/dirb.sh
+++ modules/dirb.sh
@@ -3,7 +3,7 @@
 url=$1
 furl=http://$url
 
-furl=$(python3 -c "import requests; t = requests.head('$url', allow_redirects=True).url; print(t)")
+furl=$(python3 -c "import requests; t = requests.head('$furl', allow_redirects=True).url; print(t)")
 
 echo "5) DIRECTORY BRUTE" >> /app/results/${url}-output.txt
 printf "\n\n" >> /app/results/${url}-output.txt
