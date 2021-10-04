928c23ef7c8f87ae27e1acd63beeb85db8f80170
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-04 11:59:38 +0530

    Update gather_urls.sh

--- modules/gather_urls.sh
+++ modules/gather_urls.sh
@@ -1,3 +1,4 @@
 url=$1
 
-echo "GAU" >> /app/results/$url-output.txt
+echo "6) URLS EXTRACTION " >> /app/results/$url-output.txt
+printf "\n\nLoading with more tools ...." >> /app/results/$url-output.txt
