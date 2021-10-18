37df16cf215113fc5050637f3f2790dc3b8a5ddb
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-18 21:36:54 +0530

    Update dirb.sh

--- modules/dirb.sh
+++ modules/dirb.sh
@@ -7,7 +7,7 @@ furl=http://$url
 
 furl=$(python3 -c "import requests; t = requests.head('$furl', allow_redirects=True).url; print(t)")
 
-echo "5) DIRECTORY BRUTE" >> /app/results/${url}-output.txt
+echo "8) DIRECTORY BRUTE" >> /app/results/${url}-output.txt
 printf "\n\n" >> /app/results/${url}-output.txt
 
 /app/modules/binaries/gobuster dir -q -w /app/modules/wordlists/common.txt -t 65 -u $furl | tee -a /app/results/${url}-output.txt
