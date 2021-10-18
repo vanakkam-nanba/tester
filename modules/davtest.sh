08c9a192a9e43a326faee3c3d9f41085c3a53666
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-18 21:34:27 +0530

    Update davtest.sh

--- modules/davtest.sh
+++ modules/davtest.sh
@@ -7,9 +7,9 @@ url=$1
 echo "5) WEBDAV FILE UPLOAD TEST" >> /app/results/${url}-output.txt
 printf "\n\n" >> /app/results/${url}-output.txt
 
-/app/modules/binaries/davtest $url | tee -a /app/results/${url}-output.txt
+python /app/modules/binaries/davtest $url | tee -a /app/results/${url}-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
-printf "######################################################################" >> /app/results/$url-output.txt
+printf "######################################################################\n" >> /app/results/$url-output.txt
 printf "######################################################################" >> /app/results/$url-output.txt
 printf "\n\n\n" >> /app/results/$url-output.txt
