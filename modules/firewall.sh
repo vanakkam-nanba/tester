b40f356598208babe1c833b8a901ce478596245a
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-18 21:34:53 +0530

    Update firewall.sh

--- modules/firewall.sh
+++ modules/firewall.sh
@@ -6,6 +6,6 @@ printf "\n\n" >> /app/results/${url}-output.txt
 wafw00f $url | grep "Checking" -A 100 | tee -a /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
-printf "######################################################################" >> /app/results/$url-output.txt
+printf "######################################################################\n" >> /app/results/$url-output.txt
 printf "######################################################################" >> /app/results/$url-output.txt
 printf "\n\n\n" >> /app/results/$url-output.txt
