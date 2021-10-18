4d18692436894c8be684aeb54a8b147c3d6b455e
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-10-18 21:33:49 +0530

    Update index.sh

--- modules/index.sh
+++ modules/index.sh
@@ -1,8 +1,8 @@
 url=$1
 
-printf "=========================================\n" >> /app/results/$url-output.txt
+printf "======================================\n" >> /app/results/$url-output.txt
 printf "====  REPORT OF TARGET [$url]  ==== \n" >> /app/results/$url-output.txt
-printf "=========================================\n\n" >> /app/results/$url-output.txt
+printf "======================================\n\n" >> /app/results/$url-output.txt
 
 printf "[CTRL+F AND SEARCH THE REQUIRED RESULT]\n\n" >> /app/results/$url-output.txt
 printf "==== INDEX OF REPORT ==== \n\n" >> /app/results/$url-output.txt
@@ -10,10 +10,12 @@ printf "==== INDEX OF REPORT ==== \n\n" >> /app/results/$url-output.txt
 printf "1 - DNSCAN\n" >> /app/results/$url-output.txt
 printf "2 - CLICKJACKING\n" >> /app/results/$url-output.txt
 printf "3 - CORSTEST\n" >> /app/results/$url-output.txt
-printf "4 - FIREWALL DETECTION" >> /app/results/$url-output.txt
-printf "5 - WEBDAV FILE UPLOAD TEST" >> /app/results/$url-output.txt
-printf "6 - ROBOTS.TXT CHECKER" >> /app/results/$url-output.txt
-printf "7 - SUBDOMAINS" >> /app/results/$url-output.txt
+printf "4 - FIREWALL DETECTION\n" >> /app/results/$url-output.txt
+printf "5 - WEBDAV FILE UPLOAD TEST\n" >> /app/results/$url-output.txt
+printf "6 - ROBOTS.TXT CHECKER\n" >> /app/results/$url-output.txt
+printf "7 - SUBDOMAINS\n" >> /app/results/$url-output.txt
+printf "8 - DIRECTORY BRUTE\n" >> /app/results/$url-output.txt
+printf "9 - JS FINDER + LINK FINDER\n" >> /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "######################################################################\n" >> /app/results/$url-output.txt
