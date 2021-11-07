4a58360ca0ff2811c28a01d217c9ad726889bde3
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-11-07 21:35:20 +0530

    Update sub_title_cname.sh

--- modules/sub_title_cname.sh
+++ modules/sub_title_cname.sh
@@ -5,7 +5,7 @@ url=$1
 echo "11) SUBDOMAINS [TITLE + CNAME]" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-/app/modules/binaries/httpx -silent -threads 90 -title -cname -l /app/$url-subs | tee -a /app/results/$url-output.txt
+/app/modules/binaries/httpx -silent -no-color -threads 90 -title -cname -l /app/$url-subs | tee -a /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "######################################################################" >> /app/results/$url-output.txt
