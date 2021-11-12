c6817317d8b03d326445c1afd7e31763aac2e135
vanakkam-nanba <83723671+vanakkam-nanba@users.noreply.github.com>
2021-11-12 22:26:44 +0530

    Update index.sh

--- modules/index.sh
+++ modules/index.sh
@@ -1,8 +1,8 @@
 url=$1
 
-printf "===========================================\n" >> /app/results/$url-output.txt
-printf "====  FINAL RECON REPORT OF TARGET  =======\n" >> /app/results/$url-output.txt
-printf "==========================================\n\n" >> /app/results/$url-output.txt
+printf "============================================\n" >> /app/results/$url-output.txt
+printf "======  FINAL RECON REPORT OF TARGET  ======\n" >> /app/results/$url-output.txt
+printf "============================================\n\n" >> /app/results/$url-output.txt
 
 printf "[CTRL+F AND SEARCH THE REQUIRED RESULT]\n\n" >> /app/results/$url-output.txt
 printf "==== INDEX OF REPORT [$url] ==== \n\n" >> /app/results/$url-output.txt
